e958533a65530d4f61db74668ba1affdbb27ece6
Daichi Kusawake <itemsretail@gmail.com>
2021-03-29 20:41:58 +0900

    STS4.8で作成し直したECサイト

---
+++ ecsite/src/main/java/com/internous/ecsite/model/dto/LoginDto.java
@@ -0,0 +1,65 @@
+package com.internous.ecsite.model.dto;
+
+import com.internous.ecsite.model.entity.User;
+
+public class LoginDto {
+
+	private long id;
+	private String userName;
+	private String password;
+	private String fullName;
+	
+	public LoginDto() {}
+	
+	public LoginDto(User user) {
+		this.setId(user.getId());
+		this.setUserName(user.getUserName());
+		this.setPassword(user.getPassword());
+		this.setFullName(user.getFullName());
+	}
+	
+	public LoginDto(long id,String userName,String password,String fullName) {
+		
+		this.setId(id);
+		this.setUserName(userName);
+		this.setPassword(password);
+		this.setFullName(fullName);
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	
+	
+	
+}
